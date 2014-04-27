0baf74d4b336cb8ead083f9996d51a2eabf57923
heikki <heikki@ubuntu>
2014-04-27 21:35:20 +0300

    Rumaa koodia

--- poistaaskare.php
+++ poistaaskare.php
@@ -5,14 +5,18 @@
 // to do:  tarkastus poistaessa ettei kaadu
 
 require 'models/Askare.php';
+require 'models/Kayttaja.php';
+
 session_start();
+$user = $_SESSION['kirjautunut'];
+
 
 $id = $_POST['askareId'];
 
 $testi = new Askare();
 $lista=$testi->listaaAskareet();
         foreach($lista as $askareolio)  {
-            if($askareolio->getANimi() === $id)
+            if($askareolio->getANimi() === $id && $user->getTunnus() === $askareolio->getKNimi())
             {
                 $askareolio->poistaAskareKannasta();
             }
